7760e7f61d23264e9450e6aeaf438c110c624bc3
zinogre19 <zinogre145@gmail.com>
2022-08-04 19:06:26 -0700

    Exercise 1 added

---
+++ practices/oscar/Exercise1.java
@@ -0,0 +1,8 @@
+package practices.oscar;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Hello\n Enrique Meza!");
+    }
+
+}
--- practices/oscar/Practice.java
+++ practices/oscar/Practice.java
@@ -4,3 +4,5 @@ public class Practice {
     // This is the first comment added to this class
     //>:C WHAT!
 
+
+}
