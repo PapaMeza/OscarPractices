dd5396a17f0d38912756c06abe43b7f3ee40e8dc
zinogre19 <zinogre145@gmail.com>
2022-08-12 19:30:38 -0700

    Exercise2 added

--- practices/oscar/Exercise1.java
+++ practices/oscar/Exercise1.java
@@ -3,6 +3,13 @@ package practices.oscar;
 public class Exercise1 {
     public static void main(String[] args) {
         System.out.println("Hello\n Enrique Meza!");
+
+        Exercise1.doSomething();
+    }
+
+    private static void doSomething() {
+        // This let's you do stuff
+        System.out.println("This is the method that lets me do stuff");
     }
 
 }
---
+++ practices/oscar/Exercise2.java
@@ -0,0 +1,9 @@
+package practices.oscar;
+
+public class Exercise2 {
+
+    public static void main (String[] args) {
+        System.out.println(2509+478);
+
+    }
+}
